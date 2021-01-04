39162768a2c50ebdded886a740fc2bcf5040705a
Amardeep Singh <54187793+s-amardeep@users.noreply.github.com>
2021-01-04 16:32:02 +0530

    Add files via upload

---
+++ Python Scripts/Face_Data_Collection.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### Write a Python Script that captures images from your webcam video stream
+# ### Extracts all Faces from the image frame (using haarcascades)
+# ### Stores the Face information into numpy arrays
+
+# - Read and show video stream, capture images
+# - Detect Faces and show bounding box (haarcascade)
+# - Flatten the largest face image and save in a numpy array
+# - Repeat the above for multiple people to generate training data
+
+# In[1]:
+
+
+import cv2
+import numpy as np
+
+
+# Initilising the camera
+
+# In[2]:
+
+
+cap = cv2.VideoCapture(0)
+
+
+# Face Detection
+
+# In[3]:
+
+
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")
+
+
+# In[4]:
+
+
+skip = 0
+face_data = []
+dataset_path = 'Data/'
+
+file_name = input("Enter the name of the Person : ")
+while True:
+    ret,frame = cap.read()
+
+    if ret==False:
+        continue
+
+    gray_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+
+
+    faces = face_cascade.detectMultiScale(frame,1.3,5)
+    if len(faces)==0:
+        continue
+
+    faces = sorted(faces,key=lambda f:f[2]*f[3])
+
+    # Pick the last face (because it is the largest face acc to area(f[2]*f[3]))
+    for face in faces[-1:]:
+        x,y,w,h = face
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,255),2)
+
+        #Extract (Crop out the required face) : Region of Interest
+        offset = 10
+        face_section = frame[y-offset:y+h+offset,x-offset:x+w+offset]
+        face_section = cv2.resize(face_section,(100,100))
+    
+        skip += 1
+        if skip%10==0:
+            face_data.append(face_section)
+            print(len(face_data))
+
+
+    cv2.imshow("Frame",frame)
+    cv2.imshow("Face Section",face_section)
+    
+    key_pressed = cv2.waitKey(1) & 0xFF
+    if key_pressed == ord('q'):
+        break
+        
+# Convert our face list array into a numpy array
+face_data = np.asarray(face_data)
+face_data = face_data.reshape((face_data.shape[0],-1))
+print(face_data.shape)
+
+# Save this data into file system
+np.save(dataset_path+file_name+'.npy',face_data)
+print("Data Successfully save at "+dataset_path+file_name+'.npy')
+
+cap.release()
+cv2.destroyAllWindows()
+
+
+# In[ ]:
+
+
+
+
